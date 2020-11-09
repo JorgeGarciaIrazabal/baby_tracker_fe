15edb0c935ce43e3e615134c39a14788ca40b8e6
Jorge Garcia <jorge.girazabal@gmail.com>
2020-11-08 20:49:33 -0500

    fix heroku 1

--- server.js
+++ server.js
@@ -5,11 +5,13 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist'));
+// app.use(express.static(__dirname + '/dist'));
+app.use(function (req, res, next) {
+    res.status(404).send("Sorry can't find that!")
+})
 
-app.get('/*', function(req,res) {
-
-    res.sendFile(path.join(__dirname+'/dist/index.html'));
+app.get('/*', function (req, res) {
+    res.sendFile(path.join(__dirname + '/dist/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
