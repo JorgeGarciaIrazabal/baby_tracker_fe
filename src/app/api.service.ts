35abcbbbbf7c3451e8fb1f2f6a157d26d6555221
Jorge <jirazabal@flatiron.com>
2020-11-12 02:21:32 -0500

    testing deploy

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -1,17 +1,18 @@
-import {Injectable} from '@angular/core'
-import {ApiApi} from '../openapi/apis'
-import {Configuration} from '../openapi'
-import {environment} from '../environments/environment'
+import {Injectable} from "@angular/core"
+import {ApiApi} from "../openapi/apis"
+import {Configuration} from "../openapi"
+import {environment} from "../environments/environment"
 
 @Injectable({
-    providedIn: 'root'
+    providedIn: "root"
 })
 export class ApiService {
     public api: ApiApi
 
     constructor() {
         console.log(environment)
-        const basePath = environment.production ? 'https://babytrackerbe.herokuapp.com' : 'http://localhost:9001'
+        console.log("HEYYY THIS IS NEW")
+        const basePath = environment.production ? "https://babytrackerbe.herokuapp.com" : "http://localhost:9001"
         this.api = new ApiApi(new Configuration({
                     basePath,
                 })
